d1f91a30230e4c47fae4e605f4f3c6defb50e783
yangjianbang <54735591@qq.com>
2020-11-27 20:46:49 +0800

    Default Changelist

--- service/service-edu/src/main/java/com/atguigu/serviceedu/controller/EduSubjectSelectController.java
+++ service/service-edu/src/main/java/com/atguigu/serviceedu/controller/EduSubjectSelectController.java
@@ -18,62 +18,55 @@ public class EduSubjectSelectController {
     @Autowired
     EduSubjectService subjectService;
 
-
-    //递归调用删除
-    @DeleteMapping("removeSubject/{id}")
-    public R removeSubject(@PathVariable String id) {
-        this.removeSubjectById(id);
-        return R.ok();
-    }
-
-    private void removeSubjectById(String id) {
+    //递归删除课程列表
+    @DeleteMapping("deleteSubject/{id}")
+    public R deleteSubject(@PathVariable String id) {
         List<String> idList = new ArrayList<>();
-        this.deleteChildren(id, idList);
         idList.add(id);
+        this.deleteChildren(id, idList);
         subjectService.removeByIds(idList);
-
+        return R.ok();
     }
 
     private void deleteChildren(String id, List<String> idList) {
         QueryWrapper<EduSubject> wrapper = new QueryWrapper<>();
-        wrapper.eq("parent_id", id);
-        List<EduSubject> childrenList = subjectService.list(wrapper);
-        childrenList.stream().forEach(itme -> {
-            idList.add(itme.getId());
-            this.deleteChildren(itme.getId(), idList);
+        wrapper.eq("parent_id",id);
+        List<EduSubject> children = subjectService.list(wrapper);
+        children.stream().forEach(item -> {
+            String itemId = item.getId();
+            System.out.println("itemId = " + itemId);
+            idList.add(itemId);
+            this.deleteChildren(item.getId(), idList);
         });
     }
 
-
-    //    ------------------------------------------------------
-    //递归调用获得课程列表
-    @GetMapping("getAllSubject")
-    public R getAllSubject() {
-        List<EduSubject> allList = subjectService.list(null);
-        List<EduSubject> result = this.build(allList);
+    //--------------------------------------------------------------------------------
+    //递归查询课程列表
+    @GetMapping("selectAllSubject")
+    public R selectAllSubject() {
+        List<EduSubject> alltList = subjectService.list(null);
+        List<EduSubject> result = this.build(alltList);
         return R.ok().data("result", result);
     }
 
-    private List<EduSubject> build(List<EduSubject> allList) {
-        List<EduSubject> findAll = new ArrayList<>();
-
-        allList.stream().forEach(node -> {
+    private List<EduSubject> build(List<EduSubject> alltList) {
+        List<EduSubject> oneList = new ArrayList<>();
+        alltList.stream().forEach(node -> {
             if (node.getParentId().equals("0")) {
                 node.setLevel(1);
-                findAll.add(this.selectChildren(node, allList));
+                oneList.add(this.selecyChildren(node, alltList));
             }
         });
-        return findAll;
+        return oneList;
     }
 
-    private EduSubject selectChildren(EduSubject node, List<EduSubject> allList) {
-        allList.stream().forEach(itme -> {
-            if (itme.getParentId().equals(node.getId())) {
-                itme.setLevel(node.getLevel() + 1);
-                node.getChildren().add(this.selectChildren(itme, allList));
+    private EduSubject selecyChildren(EduSubject node, List<EduSubject> alltList) {
+        alltList.stream().forEach(item -> {
+            if (item.getParentId().equals(node.getId())) {
+                item.setLevel(node.getLevel() + 1);
+                node.getChildren().add(this.selecyChildren(item, alltList));
             }
         });
         return node;
     }
-
 }
